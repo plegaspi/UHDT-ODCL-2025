7791af84fe021bdbaf6f78f898715a21b696b760
Paul Legaspi <39167788+plegaspi@users.noreply.github.com>
2024-09-17 12:35:15 -1000

    Added temporary try and except statement to prevent errors when creating shape masks

--- shape/shape_masks.py
+++ shape/shape_masks.py
@@ -2,7 +2,7 @@ import os
 import cv2 as cv
 from Color import classify_color
 if __name__ == "__main__":
-    base_dir = os.path.join('cropped_images', 'datasets', 'processed', '3-8-24 DJI Images-10')
+    base_dir = os.path.join('cropped_images', 'datasets', 'pre-processed', 'test')
     dest_dir = os.path.join('shape', 'datasets','processed', os.path.dirname(base_dir))
     if not os.path.exists(dest_dir):
         os.makedirs(dest_dir)
@@ -15,7 +15,10 @@ if __name__ == "__main__":
         base_name, file_ext = os.path.splitext(img_path)
         if file_ext in ['.png', '.jpg']:
             img = cv.imread(img_full_path)
-            img, colors, bg_masked, alphanum_masked, shape_mask, alphanum_mask = classify_color(img)
-            shape_output_path = os.path.join(dest_dir, f"{base_name}_shape{file_ext}" )
-            print(shape_output_path)
-            cv.imwrite(shape_output_path, shape_mask + alphanum_mask)
+            try: 
+                img, colors, bg_masked, alphanum_masked, shape_mask, alphanum_mask = classify_color(img)
+                shape_output_path = os.path.join(dest_dir, f"{base_name}_shape{file_ext}" )
+                print(shape_output_path)
+                cv.imwrite(shape_output_path, shape_mask + alphanum_mask)
+            except:
+                print("Error with image")
