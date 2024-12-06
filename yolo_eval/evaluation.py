493b534d435df6628a4f86c79af51988ccaadc69
Paul Legaspi <39167788+plegaspi@users.noreply.github.com>
2024-12-05 17:58:37 -1000

    Disabled inference slicer from evaluation script

--- yolo_eval/evaluation.py
+++ yolo_eval/evaluation.py
@@ -8,7 +8,7 @@ from pathlib import Path
 import shutil
 
 
-folder_path = "yolo_eval/test_images" # Enter the file path for the folder with images. Not the
+folder_path = "test-set" # Enter the file path for the folder with images. Not the
                                     # folder path with the data.yaml file.
 img_extension = ".jpg"
 model = YOLO("yolov8n.pt") # Enter the model name that you are assigned. <model_name>.pt
@@ -18,7 +18,7 @@ image_paths = glob.glob(f"{folder_path}/*{img_extension}")
 
 def callback(image_slice: np.ndarray) -> sv.Detections:
     result = model.predict(image_slice)[0]
-    return sv.Detections.from_ultralytics(result)
+    return sv.Detections.from_ultralytics(result).with_nms(0.01)
 
 test_folder_dir_path = "yolo_eval/test_results"
 shutil.rmtree(test_folder_dir_path, ignore_errors=True)
@@ -27,13 +27,13 @@ for image_path in image_paths:
     image = cv2.imread(image_path)
     orig_image = np.array(image, copy=True)
     results = model(image)[0]
-    slicer = sv.InferenceSlicer(slice_wh=(640, 640),
-                                overlap_ratio_wh=(0.1, 0.1),
-                                iou_threshold=0.001,
-                                callback = callback,
-                                thread_workers=10,
-                                )
-    detections = slicer(image)
+    #slicer = sv.InferenceSlicer(slice_wh=(640, 640),
+    #                            overlap_ratio_wh=(0.1, 0.1),
+    #                            iou_threshold=0.001,
+    #                            callback = callback,
+    #                            thread_workers=10,
+    #                            )
+    detections = callback(image)#(image)
     bounding_box_annotator = sv.BoxAnnotator()
     label_annotator = sv.LabelAnnotator()
     labels = [model.model.names[class_id] for class_id in detections.class_id]
