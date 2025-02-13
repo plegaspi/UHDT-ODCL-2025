8891d8aa5c39ebdd03b5100f14144e346c382a99
Someguy092 <124400935+Someguy092@users.noreply.github.com>
2025-02-12 20:44:11 -1000

    Update metadataExtractor2.py

--- metadataExtractor2.py
+++ metadataExtractor2.py
@@ -1,6 +1,7 @@
 from exiftool import ExifToolHelper
 
-def extractMetadata(fileName):
+def extractMetadata(fileName,sensor_w, sensor_h):
+    import math
     with ExifToolHelper() as et:
         metadata = et.get_metadata(fileName)[0]
         # print(metadata['File:Comment'])
@@ -11,8 +12,16 @@ def extractMetadata(fileName):
             altitude = metadata['EXIF:GPSAltitude']
             comment = metadata['File:Comment']
             yaw= float([component.split(":")[1] for component in comment.split() if component.startswith("yaw:")][0])
-            return metadata, latitude, longitude, altitude, yaw
+            pix_width = metadata['File:ImageWidth']
+            pix_height = metadata["File:ImageHeight"]
+            dpi_resolution = metadata['EXIF:XResolution']
+            focal_length = metadata['EXIF:FocalLength']
+            #sensor_width = pix_width/dpi_resolution
+            #sensor_height = pix_height/dpi_resolution
+            horiz_fov =  2*math.degrees(math.atan(sensor_w/focal_length))
+            vert_fov = 2*math.degrees(math.atan(sensor_h/focal_length))
+            return metadata, latitude, longitude, altitude, yaw, pix_width, pix_height, horiz_fov, vert_fov, 
         
-#metadata, latitude, longitude, altitude, yaw = extractMetadata("/home/uhdt/UAV_software/Autonomous/watchdog/image1_27.jpg")
+metadata, latitude, longitude, altitude, yaw, width, height, horiz_fov, vert_fov, = extractMetadata(r"C:\\Users\\dillo\\image10.jpg",23.55,15.6)
 
-#print(yaw)
+#print(latitude, longitude, altitude, yaw, width, height, horiz_fov, vert_fov)
