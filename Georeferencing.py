987876ca440d76bb4eaafe87dab50de3ad493edc
Someguy092 <124400935+Someguy092@users.noreply.github.com>
2025-03-24 11:51:53 -1000

    added altitude offset parameter

--- Georeferencing.py
+++ Georeferencing.py
@@ -24,12 +24,12 @@ def extractMetadata(fileName,sensor_w, sensor_h):
             vert_fov = 2*math.degrees(math.atan(sensor_h/focal_length))
             return metadata, latitude, longitude, altitude, yaw, pix_width, pix_height, horiz_fov, vert_fov, 
 
-def georeference(drone_latitude, drone_longitude, drone_altitude, drone_yaw, pix_width, pix_height, horiz_fov, vert_fov,  target_pixel_coordinates):
+def georeference(drone_latitude, drone_longitude, drone_altitude,altitude_offset drone_yaw, pix_width, pix_height, horiz_fov, vert_fov,  target_pixel_coordinates):
     # Constants for image resolution and camera field of view
     pixel_resolution = (pix_width,pix_height) # Image pixel dimensions
     horizontal_fov = horiz_fov  # Horizontal field of view in degrees
     vertical_fov = vert_fov   # Vertical field of view in degrees
-
+    drone_altitude -= altitude_offset
     # Calculate the real-world dimensions of the image at the target altitude
     image_width = 2 * drone_altitude * math.tan(math.radians(horizontal_fov / 2))
     image_height = 2 * drone_altitude * math.tan(math.radians(vertical_fov / 2))
